18feeb5845ebf3fa8d3dee8de497f42a2619aa97
Ahmed Saad <156726282+AhmedSaad912000@users.noreply.github.com>
2025-08-04 14:05:07 +0300

    added some textStyles & home blue container ui

--- lib/core/theming/styles.dart
+++ lib/core/theming/styles.dart
@@ -91,4 +91,14 @@ class TextStyles{
     fontWeight: FontWeightHelper.regular,
     color: ColorsManager.gray,
   );
+  static  TextStyle font12BlueRegular=TextStyle(
+    fontSize: 12.sp,
+    fontWeight: FontWeightHelper.regular,
+    color: ColorsManager.mainBlue,
+  );
+  static  TextStyle font18WhiteMedium=TextStyle(
+    fontSize: 18.sp,
+    fontWeight: FontWeightHelper.medium,
+    color: Colors.white,
+  );
 }
